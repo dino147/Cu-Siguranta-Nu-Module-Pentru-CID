a85662708a7ee3e4c798a769f391b60479f984b4
Teodor Sorescu <soresq@yahoo.com>
2021-04-12 23:09:30 +0300

    Update sum_8b.v

--- sum_8b.v
+++ sum_8b.v
@@ -1,14 +1,14 @@
 module sum(input[7:0] in1, in2,
-				input e,
-				output reg [7:0] out,
-				output reg co);
+	   input e,
+	   output reg [7:0] out,
+           output reg co);
 								
 
 always @(*)		
-	if (e) {co, out} = in1 + in2;
-		else begin
-			out = 0;
-			co = 0;
-		end
+   if (e) {co, out} = in1 + in2;
+   else begin
+     out = 0;
+     co = 0;
+   end
 
 endmodule
