3ab2b8cb1e8c5bf80726a17f3ddb0195c5c6278c
Teodor Sorescu <soresq@yahoo.com>
2021-04-12 22:37:56 +0300

    Create mux2_4b.v

---
+++ mux2_4b.v
@@ -0,0 +1,12 @@
+module mux(input[3:0] in0, in1,
+			  input sel,
+			  output reg[3:0] out);
+
+always @(*)
+	case (sel)
+		0: out = in0;
+		1: out = in1;
+		default: out = 0;
+	endcase
+	
+endmodule
