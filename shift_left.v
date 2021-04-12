87ffa52bab20d8f691fc5be5c925e7d12dfa9faa
Teodor Sorescu <soresq@yahoo.com>
2021-04-12 23:21:25 +0300

    Update shift_left.v

--- shift_left.v
+++ shift_left.v
@@ -1,7 +1,7 @@
 module shift_left (input [7:0] in1,
-						       input [1:0] in2,
-						       input e,
-						       output reg [7:0] out);
+		   input [1:0] in2,
+	           input e,
+	           output reg [7:0] out);
 
 always @(*)
 	if (e) out = in1 << in2;
