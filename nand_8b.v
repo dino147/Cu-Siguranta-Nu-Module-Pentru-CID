da413b98ce3313dc3692732912b0ec22063025ba
Teodor Sorescu <soresq@yahoo.com>
2021-04-12 23:08:58 +0300

    Update nand_8b.v

--- nand_8b.v
+++ nand_8b.v
@@ -1,9 +1,9 @@
 module nand(input[7:0] in1, in2,
-						   input e,
-							output reg[7:0] out);
+	    input e,
+            output reg[7:0] out);
 
 always @(*)
-	if (e) out = ~(in1 & in2);
-	else out = 0;
+   if (e) out = ~(in1 & in2);
+   else out = 0;
 
 endmodule
