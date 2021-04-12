61f9a91e51afa54a7a5bb576e7d1ccb25b6ead97
Teodor Sorescu <soresq@yahoo.com>
2021-04-12 22:42:24 +0300

    Update mux4_8b.v

--- mux4_8b.v
+++ mux4_8b.v
@@ -1,11 +1,7 @@
 module mux(input[7:0] in0, in1, in2, in3,
            input sel,
            output reg[7:0] out);
-
-   always @ (sel or in0 or in1 or in2 or in3)
-      
-endmodule
-
+           
 always @(*)
   case (sel)
     0: out = in0;
