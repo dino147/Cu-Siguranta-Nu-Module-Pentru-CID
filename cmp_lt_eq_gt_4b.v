979b2c90da3ea8859c64fd20cac764c1c27f808f
Teodor Sorescu <soresq@yahoo.com>
2021-04-12 22:44:44 +0300

    Create cmp_lt_eq_gt_4b.v

---
+++ cmp_lt_eq_gt_4b.v
@@ -0,0 +1,15 @@
+module cmp (input[3:0] a,  b,
+            output reg lt, eq, gt);
+
+  always @(*)
+    begin
+        lt = 0;
+        eq = 0;
+        gt = 0;
+        
+        if (a < b) lt = 1;
+            else if (a > b) gt = 1;
+        else eq = 1;    
+    end
+  
+endmodule
